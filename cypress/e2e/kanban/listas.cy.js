9a7ae234cc3e31d7d5f1ed0da32028ba3c5c78a1
Gabriela Gomes <gab.gomes7@outlook.com>
2025-08-13 19:50:27 -0300

    listas

--- cypress/e2e/kanban/listas.cy.js
+++ cypress/e2e/kanban/listas.cy.js
@@ -1,11 +1,11 @@
-Cypress.Commands.add('criarLista', (nome) => {
-  cy.contains('div', /Adicionar outra lista|Add another list/)
-    .should('be.visible')
-    .click();
+Cypress.Commands.add('criarLista', (nome) => { 
+    cy.contains('div', /Adicionar outra lista|Add another list/)
+      .should('be.visible')
+      .click(); 
 
-  cy.get('input[placeholder="Nova Lista"], input[placeholder="New List"]')
-    .should('be.visible')
-    .type(`${nome}{enter}`);
+    cy.get('input[placeholder="Nova Lista"], input[placeholder="New List"]')
+      .should('be.visible')
+      .type(`${nome}{enter}`); 
 
-  cy.contains(nome).should('exist');
+    cy.contains(nome).should('exist'); 
 });
